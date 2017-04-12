08e6f42c84110712abd3409d3091c03d73d22507
Ahmad Nassri <ahmad@ahmadnassri.com>
2017-04-12 09:01:37 -0400

    fix(node4): fs.constants did not exist in Node v4.x

--- index.js
+++ index.js
@@ -3,11 +3,16 @@
 const dotenv = require('dotenv')
 const fs = require('fs')
 
+const constants = {
+  F_OK: fs.constants ? fs.constants.F_OK : fs.F_OK,
+  R_OK: fs.constants ? fs.constants.R_OK : fs.R_OK
+}
+
 function load (file) {
   let readable = true
 
   try {
-    fs.accessSync(file, fs.constants.F_OK | fs.constants.R_OK)
+    fs.accessSync(file, constants.F_OK | constants.R_OK)
   } catch (e) {
     readable = false
   }
