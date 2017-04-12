11e4941347d040ea42decbef88e83a5e272bb49f
Ahmad Nassri <ahmad@ahmadnassri.com>
2017-04-12 08:52:18 -0400

    fix(core): overriding was not working in the scenario where a .env and a .env.other exist

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 'use strict'
 
 const dotenv = require('dotenv')
+const fs = require('fs')
 
-dotenv.config()
-dotenv.config({ path: `.env.${process.env.NODE_ENV}` })
+function load (file) {
+  let readable = true
+
+  try {
+    fs.accessSync(file, fs.constants.F_OK | fs.constants.R_OK)
+  } catch (e) {
+    readable = false
+  }
+
+  if (readable) {
+    Object.assign(process.env, dotenv.parse(fs.readFileSync(file)))
+  }
+}
+
+load('.env')
+load(`.env.${process.env.NODE_ENV.toLowerCase()}`)
