89d087703970e22284da4de46310f82f69140312
Ahmad Nassri <email@ahmadnassri.com>
2024-07-10 12:55:16 -0400

    test: replace tap

--- test/index.js
+++ test/index.js
@@ -1,21 +1,21 @@
-const { test } = require('tap')
+const { test } = require('node:test')
 
 const autoenv = require('../lib')
 
-test('default cwd', assert => {
-  assert.plan(1)
+test('default cwd', t => {
+  t.plan(1)
 
   autoenv()
 
-  assert.equal(process.env.FOO, undefined)
+  t.assert.equal(process.env.FOO, undefined)
 })
 
-test('custom root', assert => {
-  assert.plan(1)
+test('custom root', t => {
+  t.plan(1)
 
   process.env.NODE_ENV = 'test'
 
   autoenv(__dirname)
 
-  assert.equal(process.env.FOO, 'bar')
+  t.assert.equal(process.env.FOO, 'bar')
 })
